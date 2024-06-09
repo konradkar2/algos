752eef8565749595f4192d14a8a7888df1d83e46
Konrad Karas <konradkar2@gmail.com>
2024-06-09 21:59:27 +0200

    populate graph

--- graphDemo/main.c
+++ graphDemo/main.c
@@ -1,17 +1,29 @@
 #include "../utils/da_append.h"
+#include <ctype.h>
 #include <raylib.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+struct node;
+vector_of(struct node *, ref_node_vec);
+
+struct edge {
+  int ids[2];
+  // populated later
+  struct ref_node_vec nodes;
+};
+
+vector_of(struct edge, edge_vec);
+vector_of(struct edge *, ref_edge_vec);
 
 struct node {
   int id;
+  // populated later
   bool drawn;
+  struct ref_edge_vec edges;
 };
 
-struct edge {
-  int aId;
-  int bId;
-};
-vector_of(struct edge, edge_vec);
 vector_of(struct node, node_vec);
 
 struct Graph {
@@ -26,14 +38,14 @@ struct Graph getGraph(void) {
   da_append_safe(&graph.nodes, (struct node){.id = 100});
   da_append_safe(&graph.nodes, (struct node){.id = 101});
 
-  da_append_safe(&graph.edges, ((struct edge){.aId = 100, .bId = 101}));
+  // da_append_safe(&graph.nodes, (struct node){.id = 105});
+  // da_append_safe(&graph.nodes, (struct node){.id = 106});
+
+  da_append_safe(&graph.edges, ((struct edge){.ids = {100, 101}}));
+  // da_append_safe(&graph.edges, ((struct edge){.ids = {105, 106}}));
 
   return graph;
 }
-
-#define NODERADIUS 20
-#define EDGETHICKNESS 2
-#define NODEDISTANCE (NODERADIUS * 4)
 
 struct node *find_node(const struct node_vec *nodes, int id) {
   for (size_t i = 0; i < nodes->count; ++i) {
@@ -45,19 +57,62 @@ struct node *find_node(const struct node_vec *nodes, int id) {
   return NULL;
 }
 
+// populates references inside graph
+void prepare_graph(struct Graph *graph) {
+  for (size_t i = 0; i < graph->edges.count; ++i) {
+    struct edge *current_edge = &graph->edges.items[i];
+    for (int j = 0; j < 2; ++j) {
+      struct node *node = find_node(&graph->nodes, current_edge->ids[j]);
+      da_append_safe(&node->edges, current_edge);
+      da_append_safe(&current_edge->nodes, node);
+    }
+  }
+}
+
+#define NODERADIUS 30
+#define NODEHITBOXRADIUS (2 * NODERADIUS)
+#define EDGETHICKNESS 2
+#define NODEDISTANCE (NODERADIUS * 4)
+#define CLOCKSPLIT 24
+
+Vector2 find_pos_for_node(const struct node *node, struct Graph *graph) {
+  // for (int i = 0; i < CLOCKSPLIT; ++i) {
+  // }
+  (void)node;
+  (void)graph;
+
+  return (Vector2){0, 0};
+}
+
+void draw_node(const struct node *node, Vector2 pos) {
+  DrawCircle(pos.x, pos.y, NODERADIUS, RED);
+  Vector2 vec =
+      MeasureTextEx(GetFontDefault(), TextFormat("%d", node->id), 25, 2);
+  DrawText(TextFormat("%d", node->id), pos.x - vec.x / 2, pos.y - vec.y / 2, 25,
+           BLUE);
+}
+
 int main(void) {
   InitWindow(800, 450, "raylib [core] example - basic window");
+  SetRandomSeed(1050);
 
   while (!WindowShouldClose()) {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
+    // DrawText("Congrats! You created your first window!", 190, 200, 20,
+    //          LIGHTGRAY);
     struct Graph graph = getGraph();
+    prepare_graph(&graph);
+
     for (size_t i = 0; i < graph.edges.count; ++i) {
       struct edge *current_edge = &graph.edges.items[i];
-      struct node *node_a = find_node(&graph.nodes, current_edge->aId);
-      struct node *node_b = find_node(&graph.nodes, current_edge->bId);
+      for (int j = 0; j < 2; j++) {
+        struct node *node = find_node(&graph.nodes, current_edge->ids[j]);
+        if (!node->drawn) {
+          draw_node(node, (Vector2){50 * (i + j + 1), 50 * (i + j + 1)});
+          node->drawn = true;
+        }
+      }
     }
 
     EndDrawing();
