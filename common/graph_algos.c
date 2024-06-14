b0aec6f90b40ab491b3c403dc1f37baf0dddf38b
Konrad Karas <konradkar2@gmail.com>
2024-06-14 20:14:32 +0200

    add depth first search

--- common/graph_algos.c
+++ common/graph_algos.c
@@ -1,12 +1,40 @@
 #include "graph_algos.h"
+#include "graph.h"
+#include <limits.h>
 
-unsigned smallest_deg(struct Graph * graph)
-{
-    (void)graph;
-    return 0;
+uint get_graph_smallest_deg(struct Graph *graph) {
+  uint result = UINT_MAX;
+
+  FOR_EACH_NODE_IN_GRAPH(graph, node_it) {
+    const uint node_deg = get_node_deg(node_it);
+    if (node_deg < result) {
+      result = node_deg;
+    }
+  }
+  return result;
 }
 
-unsigned deg(const struct node * node)
-{
-    return node->edges.count;
+uint get_node_deg(const struct node *node) { return node->edges.count; }
+
+void visit_node(struct node *already_visited_node, struct node *node,
+                struct edge *edge, dfs_callback callback) {
+  node->visited = true;
+  callback(already_visited_node, node, edge);
+
+  FOR_EACH_EDGE_IN_NODE(node, edge_it) {
+    struct node *neighbour = graph_get_neighbour_of(node, *edge_it);
+    if (neighbour->visited == false) {
+      visit_node(node, neighbour, *edge_it, callback);
+    }
+  }
+}
+
+void depth_first_search(struct Graph *graph, dfs_callback cb) {
+  FOR_EACH_NODE_IN_GRAPH(graph, node_it) { node_it->visited = false; }
+
+  FOR_EACH_NODE_IN_GRAPH(graph, node_it) {
+    if (node_it->visited == false) {
+      visit_node(NULL, node_it, NULL, cb);
+    }
+  }
 }
--- common/graph_algos.h
+++ common/graph_algos.h
@@ -3,11 +3,14 @@
 
 #include "graph.h"
 
+uint get_graph_smallest_deg(struct Graph *graph);
+uint get_node_deg(const struct node *node);
 
-unsigned smallest_deg(struct Graph * graph);
-unsigned deg(const struct node * node);
+typedef void (*dfs_callback)(struct node *already_visited, struct node *visited,
+                             struct edge *edge);
+
+void depth_first_search(struct Graph *graph, dfs_callback cb);
+void dfs_visit_node(struct node *already_visited_node, struct node *node,
+                    struct edge *edge, dfs_callback callback);
 
 #endif
-
-
-
